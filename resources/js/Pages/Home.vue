cd1f533a942c4a965dac91d81b9878fa5a184830
XisoDev <ceo@amuz.co.kr>
2024-04-22 12:57:53 +0900

    first commit

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,19 @@
+<script setup>
+import {__} from "@application-install/translate.js";
+
+defineProps({
+    canLogin: Boolean,
+    canRegister: Boolean,
+});
+</script>
+
+<template>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
+                Welcome, this is new Theme.<br />
+                amuz/application-install
+            </div>
+        </div>
+    </div>
+</template>
